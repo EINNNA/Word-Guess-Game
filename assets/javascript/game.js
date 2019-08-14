993d9ce6ca682a76f68095a8b5f9a1339fd3373b
EINNNA <annie_t_@hotmail.com>
2019-08-14 23:52:52 +1000

    tried code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,25 @@
-var dogBreed = ["corgi", "beagle", "labrador", "your mom"]
+var dogBreed = ["corgi", "beagle", "bulldog", "collie", "maltese", "mastiff", "poodle", "shiba"];
 
-var letter = event.key();
+var wordGuess = dogBreed[Math.floor(Math.random() * dogBreed.length)];
 
-if letter =
+var letterGuess = document.onkeydown
+
+var unFilled = [];
+    for var i = 0; i < dogBreed.length; i++; {
+        unFilled[i] = "_ ";
+    }
+
+    document.getElementById("word").innerHTML = wordGuess
+
+    } 
+
+//LIVES
+
+if un
+
+var livesLeft = 15;
+    if (livesLeft = 0) {
+        document.textContent("YOU LOSE")
+}
+
+    if (object.onkeydown = function())
